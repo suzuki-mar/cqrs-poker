1b21a4e859d1ccf749a46a169a72204abbb6af32
suzuki-mar <supasu145@gmail.com>
2025-02-23 19:35:01 +0900

    ゲーム開始のRakeタスクを実装

--- spec/tasks/game_rake_spec.rb
+++ spec/tasks/game_rake_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe 'game:start task' do
     Rails.application.load_tasks
   end
 
-  it 'ゲームを開始できること' do
-    expect {
-      Rake::Task['game:start'].execute
-    }.not_to raise_error
+  # 標準出力は実装の詳細なので、このテストでは実行の確認のみを行う
+  it 'StartGameUseCaseを実行すること' do
+    allow(StartGameUseCase).to receive(:new).and_return(spy('StartGameUseCase'))
+    Rake::Task['game:start'].execute
+    expect(StartGameUseCase).to have_received(:new)
   end
 end 
