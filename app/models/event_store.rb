0e2ea7ee48caf9ab529dd632be3cc0f2b355d4fb
suzuki-mar <supasu145@gmail.com>
2025-02-25 11:08:59 +0900

    EventStoreに基本的なバリデーションの実装

--- app/models/event_store.rb
+++ app/models/event_store.rb
@@ -1,2 +1,5 @@
 class EventStore < ApplicationRecord
+  validates :event_type, presence: true
+  validates :event_data, presence: true
+  validates :occurred_at, presence: true
 end
---
+++ spec/models/event_store_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe EventStore, type: :model do
+  describe 'バリデーション' do
+    it { should validate_presence_of(:event_type) }
+    it { should validate_presence_of(:event_data) }
+    it { should validate_presence_of(:occurred_at) }
+  end
+end 
