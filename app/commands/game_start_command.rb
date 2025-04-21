12f9db2422e0582033eebf85950d82f4165ecd1a
suzuki-mar <supasu145@gmail.com>
2025-04-21 17:38:31 +0900

    CQRSに沿っていないのを修正

--- app/commands/game_start_command.rb
+++ app/commands/game_start_command.rb
@@ -2,16 +2,10 @@
 
 class GameStartCommand
   def execute(deck)
-    # TODO: GameStateへの依存を削除し、EventStoreを使用して状態チェックを行うように修正する
-    # CQRSの原則に従い、Command側でReadModelを参照しない
-    raise InvalidCommand, "ゲームはすでに開始されています" if GameState.exists?(status: :started)
+    if EventStoreHolder.new.game_already_started?
+      raise InvalidCommand, "ゲームはすでに開始されています"
+    end
 
-    initial_hand = deck.draw_initial_hand
-    # TODO: ReadModelの更新はProjectionで行うように修正する
-    game_state = GameState.new(status: :started)
-    game_state.assign_hand_number_from_set(initial_hand)
-    game_state.save!
-
-    initial_hand
+    deck.draw_initial_hand
   end
 end
